e36d5669ede7e2fb093fd319e07b00a7a765f5c2
Daniel Storek <dstorek@post.cz>
2014-10-14 22:55:55 +0200

    update

--- api.js
+++ api.js
@@ -15,7 +15,15 @@ var router = express.Router(); 				// get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+	// res.json({ message: 'hooray! welcome to our api!' });
+	res.json({ 
+
+		email: "my email",
+		firstname : "my firstname",
+		lastname : "my lastname",
+		passTypeId : "my passTypeId",  
+		balance : "my balance"
+	});	
 });
 
 // more routes for our API will happen here
